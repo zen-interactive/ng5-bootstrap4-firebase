d854401ffb69e1309a4c07077da32f245cd37763
wehrling <fwehrling@yahoo.fr>
2017-11-28 21:04:40 +0100

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = { 
+    production: false,
+    firebase: {
+    apiKey: "AIzaSyAVLr2kYjj25VQxf5zAS7OGQCZMGbmQKOA",
+    authDomain: "ng5-bootstrap-firebase.firebaseapp.com",
+    databaseURL: "https://ng5-bootstrap-firebase.firebaseio.com",
+    projectId: "ng5-bootstrap-firebase",
+    storageBucket: "ng5-bootstrap-firebase.appspot.com",
+    messagingSenderId: "350615259370"
+    }
+ };
